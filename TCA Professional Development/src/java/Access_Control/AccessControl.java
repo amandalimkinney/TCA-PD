54ed42e197d067142ded3c79cfb42d0ce4b36f9e
Jeff <Jeff@Jeff-PC.home>
2013-04-04 20:22:57 -0500

    Revert "Added  Development Hours"
    This reverts commit 56e0d49d643944f892ed5b894376a835a5446760.

---
+++ TCA Professional Development/src/java/Access_Control/AccessControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Access_Control;
+
+import Data_Objects.*;
+import Data_Access.*;
+
+/**
+ *
+ * @author Jeff
+ */
+public class AccessControl 
+{
+    private String accountID;
+    
+    private AccessControl()
+    {
+    }
+    
+    public Account CleanAccount()
+    {
+        return null;
+    }
+}
