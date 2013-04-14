0843d46edc0d6947a73e5e99099b6514b129d64e
ALim <amandasaysnihao@gmail.com>
2013-04-14 14:24:12 -0500

    added facades

---
+++ TCA Professional Development/src/java/Data_Objects/DevelopmentHoursFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data_Objects;
+
+import Access_control.DevelopmentHours;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Panda
+ */
+@Stateless
+public class DevelopmentHoursFacade extends AbstractFacade<DevelopmentHours> {
+    @PersistenceContext(unitName = "TCA_Professional_DevelopmentPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DevelopmentHoursFacade() {
+        super(DevelopmentHours.class);
+    }
+    
+}
---
+++ TCA Professional Development/src/java/Data_Objects/TeacherFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data_Objects;
+
+import Access_control.Teacher;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Panda
+ */
+@Stateless
+public class TeacherFacade extends AbstractFacade<Teacher> {
+    @PersistenceContext(unitName = "TCA_Professional_DevelopmentPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TeacherFacade() {
+        super(Teacher.class);
+    }
+    
+}
