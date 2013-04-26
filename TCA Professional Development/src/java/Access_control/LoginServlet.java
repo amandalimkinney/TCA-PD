a38b866dc91ca3e4d8806771c99f80afa71a1ab2
Administrator <Administrator@192.168.0.110>
2013-04-26 11:02:09 -0500

    added Register Account
    modiffied doLogout and login

--- TCA Professional Development/src/java/Access_control/LoginServlet.java
+++ TCA Professional Development/src/java/Access_control/LoginServlet.java
@@ -4,13 +4,12 @@
  */
 package Access_control;
 
-import Data_Access.UserDAO;
-import Data_Access.UserBean;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.security.*;
 
 /**
  *
@@ -18,19 +17,35 @@ import javax.servlet.http.HttpSession;
  */
     public class LoginServlet extends HttpServlet {
 
-
-        @Override
-        public void doGet(HttpServletRequest request, HttpServletResponse response) 
+        
+        
+        
+        public void processRequest(HttpServletRequest request, HttpServletResponse response) 
                     throws ServletException, java.io.IOException {
 
-            try
-            {	 
-                
-                 UserBean user = new UserBean();
+            /*try
+            {
+                String username;
+                String password;
+                username = request.getParameter("j_username").toString();
+                password = request.getParameter("j_password").toString();
+                request.login(username, password);
+                response.sendRedirect("/home/index.jsp");
+            } 
+            catch (Exception e) 
+            {
+                response.sendRedirect("loginerror.jsp");
+            }    
+                 /*UserBean user = new UserBean();
                  user.setUserName(request.getParameter("sUserName"));
                  user.setPassword(request.getParameter("sPwd"));
-
-                 user = UserDAO.login(user);
+                 
+                 HttpSession session = request.getSession(true);
+                 
+                 session.setAttribute("currentSessionUser",user); 
+                 response.sendRedirect("index.jsp"); //logged-in page 
+                 
+                 /*user = UserDAO.login(user);
 
                  if (user.isValid())
                  {
@@ -42,12 +57,107 @@ import javax.servlet.http.HttpSession;
 
                  else 
                       response.sendRedirect("loginerror.jsp"); //error page 
+                      
             } 
 
 
             catch (Throwable theException) 	    
             {
                  System.out.println(theException); 
-            }
+            }*/
+        }
+        @Override
+        public void doGet(HttpServletRequest request, HttpServletResponse response) 
+                    throws ServletException, java.io.IOException {
+/*
+            try
+            {
+                String username;
+                String password;
+                username = request.getParameter("j_username").toString();
+                password = request.getParameter("j_password").toString();
+                request.setAttribute("Repost", "False"); 
+                request.login(username, password);
+                //response.sendRedirect("/SecureMessageWebInterface/");
+            } 
+            catch (Exception e) 
+            {
+                response.sendRedirect("loginerror.jsp");
+            }    
+                 /*UserBean user = new UserBean();
+                 user.setUserName(request.getParameter("sUserName"));
+                 user.setPassword(request.getParameter("sPwd"));
+                 
+                 HttpSession session = request.getSession(true);
+                 
+                 session.setAttribute("currentSessionUser",user); 
+                 response.sendRedirect("index.jsp"); //logged-in page 
+                 
+                 /*user = UserDAO.login(user);
+
+                 if (user.isValid())
+                 {
+
+                      HttpSession session = request.getSession(true);	    
+                      session.setAttribute("currentSessionUser",user); 
+                      response.sendRedirect("index.jsp"); //logged-in page      		
+                 }
+
+                 else 
+                      response.sendRedirect("loginerror.jsp"); //error page 
+                      
+            } 
+
+
+            catch (Throwable theException) 	    
+            {
+                 System.out.println(theException); 
+            }*/
+        }
+        @Override
+        public void doPost(HttpServletRequest request, HttpServletResponse response) 
+                    throws ServletException, java.io.IOException {
+            try
+            {
+                String username;
+                String password;
+                username = request.getParameter("j_username").toString();
+                password = request.getParameter("j_password").toString();
+                request.login(username, password);
+                response.sendRedirect("/TCA_Professional_Development/home/index.jsp");
+            } 
+            catch (Exception e) 
+            {
+                response.sendRedirect("/TCA_Professional_Development/home/login.jsp?ERROR=Log-in Failed");
+            }    
+                 /*UserBean user = new UserBean();
+                 user.setUserName(request.getParameter("sUserName"));
+                 user.setPassword(request.getParameter("sPwd"));
+                 
+                 HttpSession session = request.getSession(true);
+                 
+                 session.setAttribute("currentSessionUser",user); 
+                 response.sendRedirect("index.jsp"); //logged-in page 
+                 
+                 /*user = UserDAO.login(user);
+
+                 if (user.isValid())
+                 {
+
+                      HttpSession session = request.getSession(true);	    
+                      session.setAttribute("currentSessionUser",user); 
+                      response.sendRedirect("index.jsp"); //logged-in page      		
+                 }
+
+                 else 
+                      response.sendRedirect("loginerror.jsp"); //error page 
+                      
+            } 
+
+
+            catch (Throwable theException) 	    
+            {
+                 System.out.println(theException); 
+            }*/
         }
     } 
